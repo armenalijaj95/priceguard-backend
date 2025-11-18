f55d81ae5dedc1b87469e4f8ac7721c66012318e
armen <armenalijaj2012@gmail.com>
2025-11-19 00:59:27 +0100

    Final bulletproof CORS fix

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ import trackRoute from "./routes/track.js";
 
 const app = express();
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
     if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
+        return res.status(200).end();
     }
 
     next();
