e96276bcdf51e429c1af4dbd2465c3714e432cc4
armen <armenalijaj2012@gmail.com>
2025-11-19 00:49:50 +0100

    Full CORS fix for Chrome extension

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ import { connectDB } from "./db.js";
 import trackRoute from "./routes/track.js";
 
 const app = express();
-app.use(
-    cors({
-        origin: "*",
-        methods: ["GET", "POST"],
-        allowedHeaders: ["Content-Type"],
-    })
-);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
 app.use(express.json());
 
 connectDB();
